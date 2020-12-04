60554bd9cd2bfd1492025cc58cee48f5438a3a8d
Jingrong Chen <crazyboycjr@gmail.com>
2020-12-04 16:51:16 -0500

    improve the greedy algorithm

--- examples/mapreduce.rs
+++ examples/mapreduce.rs
@@ -5,7 +5,7 @@ use std::collections::HashSet;
 use std::rc::Rc;
 
 use nethint::bandwidth::{Bandwidth, BandwidthTrait};
-use nethint::cluster::{Cluster, Node, NodeIx, NodeType, Topology};
+use nethint::cluster::{Cluster, Node, NodeType, Topology};
 use nethint::{
     AppEvent, Application, Event, Executor, Flow, Simulator, ToStdDuration, Trace, TraceRecord,
 };
@@ -297,11 +297,7 @@ impl<'a, 'ctx> Unit for GAUnit<'a, 'ctx> {
     }
 
     fn breed_with(&self, other: &GAUnit) -> Self {
-        let GAContext {
-            succ,
-            cross,
-            ..
-        } = self.ctx;
+        let GAContext { succ, cross, .. } = self.ctx;
 
         // crossover
         let mut result = self.crossover(other);
@@ -413,21 +409,44 @@ impl PlaceReducer for GreedyReducerScheduler {
         // second, find the best node in that rack
         let mut placement = Vec::new();
         let num_racks = cluster.num_switches() - 1;
+
+        // number of nodes has been taken in a rack
         let mut rack_taken = vec![0; num_racks];
+
+        // number of reducers in the rack
         let mut rack = vec![0; num_racks];
 
-        let mut taken: HashSet<NodeIx> =
-            mapper.0.iter().map(|m| cluster.get_node_index(m)).collect();
+        // existing ingress traffic to rack_i during greedy
+        let mut ingress = vec![0; num_racks];
+
+        let mut taken: HashSet<_> = mapper.0.iter().map(|m| cluster.get_node_index(m)).collect();
         mapper.0.iter().for_each(|m| {
             let rack_id = get_rack_id(cluster, m);
             rack_taken[rack_id] += 1;
         });
 
-        for j in 0..job_spec.num_reduce {
+        // reducer rank sorted by weight
+        let mut reducer_weight = vec![0; job_spec.num_reduce];
+        debug_assert!(job_spec.num_map <= shuffle_pairs.0.len());
+        for i in 0..job_spec.num_map {
+            debug_assert!(job_spec.num_map <= shuffle_pairs.0[i].len());
+            for j in 0..job_spec.num_reduce {
+                reducer_weight[j] += shuffle_pairs.0[i][j];
+            }
+        }
+
+        use std::cmp::Reverse;
+        let mut rank: Vec<_> = (0..job_spec.num_map).collect();
+        rank.sort_by_key(|&r| Reverse(reducer_weight[r]));
+
+        for j in rank {
             let mut min_est = f64::MAX;
+            let mut min_traffic = usize::MAX;
             let mut best_rack = 0;
 
+            // Step 1. find the best rack
             for i in 0..num_racks {
+                let mut traffic = 0;
                 let mut est = 0.;
                 let tor_ix = cluster.get_node_index(&format!("tor_{}", i));
                 let link_ix = cluster.get_uplink(tor_ix);
@@ -441,26 +460,28 @@ impl PlaceReducer for GreedyReducerScheduler {
 
                 for (mi, m) in mapper.0.iter().enumerate() {
                     let m = cluster.get_node_index(&m);
-                    // let m_bw = cluster[cluster.get_uplink(m)].bandwidth;
                     let rack_m = cluster.get_target(cluster.get_uplink(m));
                     if rack_m != tor_ix {
-                        est +=
-                            (shuffle_pairs.0[mi][j] * (rack[i] + 1)) as f64 / rack_bw.val() as f64;
+                        // est += (shuffle_pairs.0[mi][j] * (rack[i] + 1)) as f64 / rack_bw.val() as f64;
+                        est += (shuffle_pairs.0[mi][j] + ingress[i]) as f64 / rack_bw.val() as f64;
+                        traffic += shuffle_pairs.0[mi][j];
                     }
                 }
 
                 if min_est > est {
                     min_est = est;
+                    min_traffic = traffic;
                     best_rack = i;
                 }
             }
 
             // get the best_rack
             rack[best_rack] += 1;
+            ingress[best_rack] += min_traffic;
             rack_taken[best_rack] += 1;
-            // debug!("best_rack: {}", best_rack);
+            debug!("best_rack: {}, traffic: {}", best_rack, ingress[best_rack]);
 
-            // fix the rack, find the best node in the rack
+            // Step 2. fix the rack, find the best node in the rack
             let tor_ix = cluster.get_node_index(&format!("tor_{}", best_rack));
             let downlink = cluster
                 .get_downlinks(tor_ix)
@@ -624,7 +645,7 @@ fn main() {
     assert_eq!(cluster.num_hosts(), nports * nports * nports / 4);
     info!("cluster:\n{}", cluster.to_dot());
 
-    let job_spec = JobSpec::new(32, 32);
+    let job_spec = JobSpec::new(20, 20);
 
     let mut data = Vec::new();
     for i in 0..10 {
