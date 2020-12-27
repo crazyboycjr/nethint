77a69f973b423ff6db6addef3b982d94280e6fa6
Danyang Zhuo <danyangzhuo@gmail.com>
2020-12-26 22:25:28 -0500

    fix

--- run_allreduce.sh
+++ run_allreduce.sh
@@ -6,7 +6,7 @@ mkdir -p results
 for nethint in 0 1;
 do
   for cjobs in 4 8 16; do
-    for test_index in 0; do
+    for test_index in 0 1 2 3 4; do
       RUST_LOG=info cargo run -- -n $cjobs -h $nethint arbitrary 150 8 100 200 > results/nethint-$nethint-jobs-$cjobs-$test_index.txt &
     done
   done
