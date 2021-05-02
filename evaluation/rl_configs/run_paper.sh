27afca9b55b9ffdc8a0afd0c478217bda07144c0
Jingrong Chen <crazyboycjr@gmail.com>
2021-05-02 01:57:47 -0400

    update broadcast simulation setting

---
+++ evaluation/rl_configs/run_paper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
+
+configs=(
+standard2.toml
+standard3.toml
+background_dynamic_strong.toml
+background_off.toml
+background_static_strong.toml
+level2bad.toml
+)
+
+for conf in ${configs[@]}; do
+	echo $conf
+	RUST_BACKTRACE=1 RUST_LOG=error cargo run --bin rl_experiment --release -- -P 5 -c $conf &
+done
+
+wait
